2a922b44c593b1135eeed7cd37eb8f0455e17d41
canadianbees <calzenor@outlook.com>
2024-01-10 11:13:53 -0500

    make utility files

---
+++ src/app/lib/utils.ts
@@ -0,0 +1,11 @@
+//  twMerge combines tailwind classes into one (optimization)
+// left-0 right-0 -> inset-x-0
+
+import { ClassValue, clsx } from 'clsx'
+import { twMerge } from 'tailwind-merge'
+
+//allows conditional classes to be written
+export function cn(...inputs: ClassValue[])
+{
+    return twMerge(clsx(inputs))
+}
