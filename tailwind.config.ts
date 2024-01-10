33aed2a128dd5839c9ffcaca3103d29245ce0e91
canadianbees <calzenor@outlook.com>
2024-01-10 11:14:50 -0500

    install deps

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,8 @@
 import type { Config } from 'tailwindcss'
+const withMT = require("@material-tailwind/react/utils/withMT");
 
-const config: Config = {
+
+const config: Config = withMT({
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -15,6 +17,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
-}
+  plugins: [require("daisyui"), require("@tailwindcss/forms")],
+})
 export default config
