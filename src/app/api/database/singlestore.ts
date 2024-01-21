8200d97ad1d489133b59d81e13070865d10a4166
canadianbees <calzenor@outlook.com>
2024-01-21 17:08:30 -0500

    update env vars, make query functions

--- src/app/api/database/singlestore.ts
+++ src/app/api/database/singlestore.ts
@@ -1,11 +1,9 @@
-import { connect } from "http2";
 import mysql from "mysql2/promise";
 
 const HOST = process.env.HOST;
 const PASSWORD = process.env.PASSWORD;
 const USER = process.env.USER;
-const SQL_DATABASE = process.env.SQL_DATABASE;
-const VEC_DATABASE = process.env.VEC_DATABASE;
+const DATABASE = process.env.DATABASE;
 
 if (!HOST || !PASSWORD || !USER) {
   throw Error("db info scuffed");
@@ -21,7 +19,7 @@ export async function connectSingleStore() {
       host: HOST,
       user: USER,
       password: PASSWORD,
-      database: VEC_DATABASE,
+      database: DATABASE,
     });
     return singleStoreConnection;
   } catch (err) {
@@ -51,14 +49,29 @@ export async function findRelevantDocs({
 
     //SQL QUERY - takes the embedding, converts it to a vector singlestore recognizes,
     //  does the dot product between that vector and all the vectors in the table
+    // returns the content id
+    const res: any = await getContentID(conn, embedding);
+    const con_id: string = res[0].content_id;
+
     const query =
-      "SELECT title, text, DOT_PRODUCT_F64(JSON_ARRAY_PACK_F64('[" +
-      embedding +
-      "]'), content_vector) AS score FROM wikipedia ORDER BY score DESC LIMIT 1";
+      "SELECT content, doc_id FROM contents WHERE content_id = '" +
+      con_id +
+      "'";
 
-    const result = await conn.query(query);
-    return result[0];
+    const cont_result: any = await conn.query(query);
+    const doc_id = cont_result[0][0].doc_id;
+    const doc_result: any = await getProject(conn, doc_id);
+    const source_id = doc_result[0].source_id;
+    const source_result: any = await getSource(conn, source_id )
 
+    const relevantDoc = {
+      project_title: doc_result[0].title,
+      content: cont_result[0][0].content,
+      total_issues: doc_result[0].num_segments,
+      project_status: doc_result[0].status,
+      source: source_result[0].source_type
+    }
+    return relevantDoc;
   } catch (error) {
     console.log(error);
     return error;
@@ -70,31 +83,49 @@ export async function findRelevantDocs({
   }
 }
 
-export async function searchDatabase({
-  conn,
-  embedding,
-}: {
-  conn?: mysql.Connection;
-  embedding: any;
-}) {
-  try {
-    if (!conn) {
-      conn = await connectSingleStore();
-    }
+//
 
-    let query =
-      "SELECT title, text, DOT_PRODUCT_F64(JSON_ARRAY_PACK_F64('[" +
-      embedding +
-      "]'), content_vector) AS score FROM wikipedia ORDER BY score DESC LIMIT 5";
+async function getContentID(conn: mysql.Connection, embedding: any) {
+  const query =
+    "SELECT content_id, DOT_PRODUCT_F64(JSON_ARRAY_PACK_F64('[" +
+    embedding +
+    "]'), embedding) AS score FROM embeddings_table ORDER BY score DESC LIMIT 1";
 
-    const result = await conn.query(query);
-    return result[0];
-  } catch (error) {
-    console.log(error);
-    return error;
-  } finally {
-    if (conn) {
-      await stopSingleStore(conn);
-    }
-  }
+  const result = await conn.query(query);
+  return result[0];
 }
+
+async function getProject(conn: mysql.Connection, doc_id: string) {
+  const query = "SELECT title, num_segments, status, source_id FROM documents WHERE doc_id = '" + doc_id + "'";
+  const result = await conn.query(query);
+  return result[0];
+}
+
+async function getSource(conn: mysql.Connection, source_id: string) {
+  const query = "SELECT source_type FROM data_source WHERE source_id = '" + source_id + "'";
+  const result = await conn.query(query);
+  return result[0];
+}
+
+//export async function searchDatabase({
+//   conn,
+//   embedding,
+// }: {
+//   conn?: mysql.Connection;
+//   embedding: any;
+// }) {
+//   try {
+//     if (!conn) {
+//       conn = await connectSingleStore();
+//     }
+
+//   return result[0];
+//   } catch (error) {
+//     console.log(error);
+//     return error;
+//   } finally {
+//     if (conn) {
+//       await stopSingleStore(conn);
+//     }
+//   }
+// }
