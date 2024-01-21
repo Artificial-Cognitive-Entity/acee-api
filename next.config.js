cd0c301dcca232edbfe2cc58ad4a84fabc04b515
canadianbees <calzenor@outlook.com>
2024-01-21 17:00:24 -0500

    install packages

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+      webpack: (config) => {
+        config.resolve.fallback = { request: false };
+    
+        return config;
+      },
+}
 
 module.exports = nextConfig
