3d04418e91429314cfb318d2c0b75b292ec01f4c
canadianbees <calzenor@outlook.com>
2024-01-21 17:09:17 -0500

    switch embedding model, update system message

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,7 +1,8 @@
 import { ChatCompletionMessage } from "openai/resources/index.mjs";
-import openai, { getEmbedding } from "@/app/lib/openai";
+import openai from "@/app/lib/openai";
 import { findRelevantDocs } from "@/app/api/database/singlestore";
 import { OpenAIStream, StreamingTextResponse } from "ai";
+import { geckoEmbedding } from "@/app/lib/vertexai";
 
 export async function POST(req: Request) {
   try {
@@ -12,7 +13,7 @@ export async function POST(req: Request) {
     // get last 6 messages (user and ai) for context
     const messagesTruncated = messages.slice(-6);
 
-    const embedding = await getEmbedding(
+    const embedding = await geckoEmbedding(
       messagesTruncated.map((message) => message.content).join("\n")
     );
 
@@ -20,32 +21,27 @@ export async function POST(req: Request) {
     const relevantDocs: any = await findRelevantDocs({ embedding });
     let systemMessage: ChatCompletionMessage;
 
-    if(relevantDocs)
-    {
+    if (relevantDocs) {
       systemMessage = {
         role: "assistant",
-        content:
-          "You are an AI assisant built to aid employees in finding relevant documentation pertaining to the company. You are able to answer the user's questions based on documents in the database. Do not use any sources other than the relevant documents provided." +
-          "The relevant documents found in the database for this query are:\n" +
-          relevantDocs
-            .map(
-              (doc: { text: string; title: string }) =>
-                `Title: ${doc.title}\n\nContent:\n${doc.text}`
-            )
-            .join(),
+        content: `You are an AI assisant built to aid employees in finding relevant documentation pertaining to the company. 
+        You are able to answer the user's questions based on documents in the database. Do not use any sources other than the relevant documents provided.
+        The relevant documents found in the database for this query are:\n\n
+        
+        Project Title: ${relevantDocs.title}
+        Number of Issues: ${relevantDocs.total_issues}
+        Project Status: ${relevantDocs.project_status}
+        Content: ${relevantDocs.content}`,
       };
-    }
-
-    else{
+    } else {
       systemMessage = {
         role: "assistant",
-        content:
-          "You are an AI assisant built to aid employees in finding relevant documentation pertaining to the company. You are able to answer the user's questions based on documents in the database. Do not use any sources other than the relevant documents provided." +
-          "Unfortunately, there are no relevant documents available, so let the user know that you are unable to help them at this moment " 
+        content: `You are an AI assisant built to aid employees in finding relevant documentation pertaining to the company.
+        You are able to answer the user's questions based on documents in the database.
+        Do not use any sources other than the relevant documents provided. 
+        Unfortunately, there are no relevant documents available, so let the user know that you are unable to help them at this moment.`,
       };
-
     }
-   
 
     const response = await openai.chat.completions.create({
       model: "gpt-3.5-turbo",
