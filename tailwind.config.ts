6dbff31de5608e9905fbd32ff07b34e13a115c9d
canadianbees <calzenor@outlook.com>
2024-02-08 21:20:19 -0500

    add dependencies

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,22 +1,21 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 const withMT = require("@material-tailwind/react/utils/withMT");
-
 
 const config: Config = withMT({
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-
-    },
-    daisyui:{
-      themeRoot: ":root",
-      themes:["wireframe", "cmyk"],
-    }
+    extend: {animation: {
+      'pulse': 'pulse 1.5s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+    }},
+  },
+  daisyui: {
+    themeRoot: ":root",
+    themes: ["business", "wireframe", "cmyk"],
   },
   plugins: [require("daisyui"), require("@tailwindcss/forms")],
-})
-export default config
+});
+export default config;
