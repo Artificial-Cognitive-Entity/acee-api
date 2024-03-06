cc6fbe770e1db7703a2060666d1079080ab621dc
canadianbees <calzenor@outlook.com>
2024-03-06 14:36:33 -0500

    formatting

--- src/app/lib/utils.ts
+++ src/app/lib/utils.ts
@@ -1,11 +1,10 @@
 //  twMerge combines tailwind classes into one (optimization)
 // left-0 right-0 -> inset-x-0
 
-import { ClassValue, clsx } from 'clsx'
-import { twMerge } from 'tailwind-merge'
+import { ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 //allows conditional classes to be written
-export function cn(...inputs: ClassValue[])
-{
-    return twMerge(clsx(inputs))
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
 }
