e0a57666dfa925206496e09cf58f1e29916304cf
canadianbees <calzenor@outlook.com>
2024-03-05 19:51:36 -0500

    clean up file, update find document function

--- src/app/api/database/singlestore.ts
+++ src/app/api/database/singlestore.ts
@@ -53,7 +53,6 @@ export async function findEmail({
     const query: any = `SELECT * FROM users WHERE email = "${email.toLowerCase()}"`;
     const result: any = await conn.query(query);
 
-    console.log(result[0]);
     if (result) {
       return result[0];
     } else {
@@ -91,8 +90,6 @@ export async function addUser({
       roles: [role],
     };
 
-    
-
     // get date created in EST
     const offset = -300;
     const date: string = new Date(new Date().getTime() + offset * 60 * 1000)
@@ -124,6 +121,7 @@ export async function addUser({
 }
 
 //finds the most relevant document to the user's query
+// used for chatting
 export async function findRelevantDocs({
   conn,
   embedding,
@@ -143,12 +141,10 @@ export async function findRelevantDocs({
     const embedQuery =
       "SELECT content_id, DOT_PRODUCT_F64(JSON_ARRAY_PACK_F64('[" +
       embedding +
-      "]'), embedding) AS score FROM embeddings_table ORDER BY score DESC LIMIT 1";
+      "]'), embedding) AS score FROM embeddings ORDER BY score DESC LIMIT 1";
 
-    const res: any = await getContentID(conn, embedQuery);
-
-    const con_id: string = res[0].content_id;
-    const relevantDoc = await getDocs(conn, con_id);
+    const res: any = await getIDs(conn, embedQuery, "chat");
+    const relevantDoc = await getDocument(conn, res.content_id, "chat");
     return relevantDoc;
   } catch (error) {
     console.log(error);
@@ -190,22 +186,25 @@ export async function searchDatabase({
     const embedQuery =
       "SELECT content_id, DOT_PRODUCT_F64(JSON_ARRAY_PACK_F64('[" +
       embedding +
-      "]'), embedding) AS score FROM embeddings_table ORDER BY score DESC LIMIT 10 ";
+      "]'), embedding) AS score FROM embeddings ORDER BY score DESC LIMIT 10 ";
 
-    const res: any = await getContentID(conn, embedQuery);
+    const res: any = await getIDs(conn, embedQuery, "search");
 
     let projects: any = [];
 
-    for (let i = 0; i < 10; i++) {
-      if (res[i].score > 0.6) {
-        const con_id: string = res[i].content_id;
-        const document = await getDocs(
+    for (let i = 0; i < res.length; i++) {
+      if (res[i].score > 0.5) {
+        // const document = await getDocument(
+        //   conn,
+        //   res[i],
+        //   parsedInput.result[0].filter.type
+        // );
+        await groupByParent(
+          projects,
           conn,
-          con_id,
-          true,
-          parsedInput.result[0].source
+          res[i],
+          parsedInput.result[0].filter.type
         );
-        await getJiraContent(document, projects);
       }
     }
     return projects;
@@ -219,340 +218,166 @@ export async function searchDatabase({
   }
 }
 
-//get and format doc into json
-async function getDocs(
+// to group jira issues by project
+async function groupByParent(
+  projArr: any,
   conn: mysql.Connection,
-  con_id: string,
-  hybrid?: boolean,
+  result: any,
+  filter?: string
+) {
+  
+  const document = await getDocument(conn, result, "search", filter);
+
+  // TODO: GROUP DOCUMENTS BY THEIR PARENT
+  // CHECK IF DOCUMENT IS A PARENT (PARENT_ID == NULL)
+  // IF DOCUMENT IS A PARENT, CHECK IF IT HAS A CHILDREN
+  // IF PARENT, ADD THE NODE TO THE PROJECT
+
+  // IF DOCUMENT IS A PARENT, MAKE DOCUMENT A ROOT KEY, ADD CHILDREN ARRAY
+  // IF DOCUMENT IS A CHILD, IT MUST HAVE A PARENT_ID.
+  // GET PARENT_ID AND ADD IT TO THE CORRECT PLACE IN THE PROJECT
+
+
+
+
+  /*
+
+
+  [
+    {
+        "node_title": "Security Operations Center",
+        "node_type": "space",
+
+        "children": [
+          {
+              "node_title": "Security Operations Center Home",
+              "node_type": "page",
+              "children"
+
+
+          }
+
+
+
+
+        ]
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+  ]
+  */
+}
+
+//get and format doc into json
+async function getDocument(
+  conn: mysql.Connection,
+  result: any,
+  type: string,
   filter?: string
 ) {
   let query;
-  if (hybrid) {
-    query =
-      "SELECT content, doc_id FROM contents WHERE content_id = '" +
-      con_id +
-      "'";
-  } else {
-    query =
-      "SELECT content, doc_id FROM contents WHERE content_id = '" +
-      con_id +
-      "'";
+
+  const content_id = result.content_id;
+  const node_id = result.node_id;
+
+  // if (filter) {
+  //   query = `
+  //   SELECT content, content_type FROM contents WHERE content_id = '"${content_id}"' AND content_type = '"${filter}"'
+  //   `;
+  // } else {
+  //   query = `
+  //   SELECT content, content_type FROM contents WHERE content_id = '"${content_id}"'`;
+  // }
+
+  query = `
+  SELECT content, content_type FROM contents WHERE content_id = '"${content_id}"'`;
+  let content: any = await getContent(conn, query);
+
+  let node: any = await getNode(conn, node_id);
+
+  if (type == "search") {
+    return {
+      node_title: node.node_title.replace(/["]+/g, ""),
+      node_type: node.node_type.replace(/["]+/g, ""),
+      content: content.content.replace(/["]+/g, ""),
+      content_type: content.content_type.replace(/["]+/g, ""),
+      url: node.url.replace(/["]+/g, ""),
+      data_source: node.data_source.replace(/["]+/g, ""),
+      last_updated: node.last_updated.replace(/["]+/g, ""),
+      parent_id: node.parent_id.replace(/["]+/g, ""),
+      children_ids: node.parent_id.replace(/["]+/g, ""),
+    };
   }
 
-  let cont_result: any = await conn.query(query);
-  let doc_id = cont_result[0][0].doc_id;
-  let doc_result: any = await getProject(conn, doc_id);
-  let source_id = doc_result[0].source_id;
-  let source_result: any = await getSource(conn, source_id);
-
-  const relevantDoc = {
-    project_title: doc_result[0].title.replace(/["]+/g, ""),
-    content: cont_result[0][0].content.replace(/["]+/g, ""),
-    total_issues: doc_result[0].num_segments,
-    project_status: doc_result[0].status.replace(/["]+/g, ""),
-    source: source_result[0].source_type.replace(/["]+/g, ""),
+  return {
+    node_title: node.node_title.replace(/["]+/g, ""),
+    node_type: node.node_type.replace(/["]+/g, ""),
+    content: content.content.replace(/["]+/g, ""),
+    content_type: content.content_type.replace(/["]+/g, ""),
+    url: node.url.replace(/["]+/g, ""),
+    data_source: node.data_source.replace(/["]+/g, ""),
+    last_updated: node.last_updated.replace(/["]+/g, ""),
   };
-
-  return relevantDoc;
 }
+
+async function getContent(conn: mysql.Connection, query: string) {
+  const content: any = await conn.query(query);
+  return content[0][0];
+}
+
+async function getNode(conn: mysql.Connection, node_id: string) {
+  const query = `
+  SELECT node_type, node_title, url, data_source, last_updated, parent_id, children_ids FROM nodes WHERE node_id = '"${node_id}"'
+  `;
+  const result: any = await conn.query(query);
+  return result[0][0];
+}
+
 //utility functions
-async function getContentID(conn: mysql.Connection, query: string) {
-  const result = await conn.query(query);
-  return result[0];
-}
-async function getProject(conn: mysql.Connection, doc_id: string) {
-  const query =
-    "SELECT title, num_segments, status, source_id FROM documents WHERE doc_id = '" +
-    doc_id +
-    "'";
-  const result = await conn.query(query);
-  return result[0];
-}
-async function getSource(conn: mysql.Connection, source_id: string) {
-  const query =
-    "SELECT source_type FROM data_source WHERE source_id = '" + source_id + "'";
-  const result = await conn.query(query);
-  return result[0];
-}
+async function getIDs(conn: mysql.Connection, query: string, type: string) {
+  const res: any = await conn.query(query);
+  const search_result = res[0];
+  let content_id = "";
+  let node_id = "";
 
-function searchForSprint(sprints: any, target: string) {
-  if (sprints.length > 0) {
-    return sprints.find((sprint: any) => {
-      return sprint.sprint_name == target;
-    });
-  }
-  return false;
-}
+  // return is different based on whether this is for a search or  chat function
+  let result = [];
 
-function searchForIssue(issues: any, target: string) {
-  if (issues.length > 0) {
-    return issues.find((issue: any) => {
-      return issue.issue_title == target;
-    });
-  }
-  return false;
-}
+  if (type == "search") {
+    // get the node id by splitting by the dash, return the sim. score
+    // extract the node id from the content id
+    for (let i = 0; i < search_result.length; i++) {
+      // get content id
+      content_id = search_result[i].content_id.replace(/["]+/g, "");
 
-function searchForBoard(boards: any, target: string) {
-  if (boards.length > 0) {
-    return boards.find((board: any) => {
-      return board.board_name == target;
-    });
-  }
-  return false;
-}
-function addIssue(
-  boards: any,
-  issue_title: string,
-  issue_type: string,
-  issue_description: string,
-  issue_created: string,
-  issue_status: string,
-  board_name: string,
-  sprint_name: string
-) {
-  const issue_info = {
-    issue_title: issue_title,
-    issue_type: issue_type,
-    issue_desc: issue_description,
-    issue_created: issue_created,
-    issue_status: issue_status,
-  };
+      //extract node id from content_id
+      node_id = search_result[i].content_id.split("-")[0].replace(/["]+/g, "");
 
-  const containsBoard = searchForBoard(boards, board_name);
-
-  // find the board it belongs to
-  if (containsBoard) {
-    const boardIndx = boards.indexOf(containsBoard);
-    const containsSprint = searchForSprint(
-      boards[boardIndx].sprints,
-      sprint_name
-    );
-
-    // find the sprint it belongs to
-    if (containsSprint) {
-      const sprintIndex = boards[boardIndx].sprints.indexOf(containsSprint);
-
-      const containsIssue = searchForIssue(
-        boards[boardIndx].sprints[sprintIndex].issues,
-        issue_title
-      );
-
-      // if the issue doesnt exist add it
-      if (!containsIssue) {
-        boards[boardIndx].sprints[sprintIndex].issues.push(issue_info);
-      }
-    }
-  }
-}
-function addSprint(boards: any, sprint_name: string, board_name: string) {
-  const sprint_info: any = {
-    sprint_name: sprint_name,
-  };
-
-  sprint_info["issues"] = [];
-
-  const containsBoard = searchForBoard(boards, board_name);
-
-  // find the board it belongs to
-  if (containsBoard) {
-    const boardIndx = boards.indexOf(containsBoard);
-    const containsSprint = searchForSprint(
-      boards[boardIndx].sprints,
-      sprint_info.sprint_name
-    );
-
-    // if sprint isn't already in the sprint array, add it
-    if (!containsSprint) {
-      boards[boardIndx].sprints.push(sprint_info);
-    }
-  }
-}
-function addBoard(board_name: string, boards: any) {
-  // get info for board and push it into the boards array
-  const board_info: any = {
-    board_name: board_name,
-  };
-
-  board_info["sprints"] = [];
-
-  // if sprint isn't already in the sprint array, add it
-  const containsBoard = boards.find((board: { board_name: any }) => {
-    return board.board_name === board_info.board_name;
-  });
-
-  if (!containsBoard) {
-    boards.push(board_info);
-  }
-}
-// to group jira issues by project
-async function getJiraContent(document: any, projArr: any) {
-  let response: any = await ContentTranslator().translate(document.content);
-  const parsedContent = JSON.parse(JSON.stringify(response.data, undefined, 3)); // turn it into a JSON object
-  const project_name = parsedContent.result[0].project_name;
-  const project_source = document.source;
-  const board_name = parsedContent.result[0].board_name;
-  const sprint_name = parsedContent.result[0].sprint_name;
-  const issue_title = parsedContent.result[0].issue_title;
-  const issue_type = parsedContent.result[0].issue_type;
-  const issue_description = parsedContent.result[0].issue_description;
-  const issue_created = parsedContent.result[0].issue_created;
-  const issue_status = parsedContent.result[0].issue_status;
-
-  //add the first project
-  if (projArr.length == 0) {
-    projArr.push(
-      makeProject(
-        sprint_name,
-        board_name,
-        project_name,
-        project_source,
-        document.project_status,
-        issue_title,
-        issue_type,
-        issue_description,
-        issue_created,
-        issue_status
-      )
-    );
-
-    return;
-  }
-
-  //search for the project
-  const containsProject: any = projArr.find(
-    (project: { project_title: any }) => project.project_title === project_name
-  );
-
-  //check if project already exists, search for the board
-  if (containsProject) {
-    const projectIndex = projArr.indexOf(containsProject);
-    const containsBoard: any = searchForBoard(
-      projArr[projectIndex].project_info.boards,
-      board_name
-    );
-
-    // if board already exists, search for the sprint
-    if (containsBoard) {
-      const boardIndx =
-        projArr[projectIndex].project_info.boards.indexOf(containsBoard);
-
-      const containsSprint: any = searchForSprint(
-        projArr[projectIndex].project_info.boards[boardIndx].sprints,
-        sprint_name
-      );
-
-      //if sprint already exists, add the issue
-      if (containsSprint) {
-        addIssue(
-          projArr[projectIndex].project_info.boards,
-          issue_title,
-          issue_type,
-          issue_description,
-          issue_created,
-          issue_status,
-          board_name,
-          sprint_name
-        );
-      }
-
-      //make a new sprint
-      else {
-        addSprint(
-          projArr[projectIndex].project_info.boards,
-          sprint_name,
-          board_name
-        );
-        addIssue(
-          projArr[projectIndex].project_info.boards,
-          issue_title,
-          issue_type,
-          issue_description,
-          issue_created,
-          issue_status,
-          board_name,
-          sprint_name
-        );
-      }
+      result.push({
+        node_id: node_id,
+        content_id: content_id,
+        score: search_result[i].score,
+      });
     }
 
-    //board does not exist, add new board
-    else {
-      addBoard(board_name, projArr[projectIndex].boards);
-      addSprint(
-        projArr[projectIndex].project_info.boards,
-        sprint_name,
-        board_name
-      );
-      addIssue(
-        projArr[projectIndex].project_info.boards,
-        issue_title,
-        issue_type,
-        issue_description,
-        issue_created,
-        issue_status,
-        board_name,
-        sprint_name
-      );
-    }
+    return result;
   }
 
-  //the project does not exist so create it
-  else {
-    projArr.push(
-      makeProject(
-        sprint_name,
-        board_name,
-        project_name,
-        project_source,
-        document.project_status,
-        issue_title,
-        issue_type,
-        issue_description,
-        issue_created,
-        issue_status
-      )
-    );
-  }
-}
+  content_id = search_result[0].content_id.replace(/["]+/g, "");
+  node_id = search_result[0].content_id.split("-")[0].replace(/["]+/g, "");
 
-function makeProject(
-  sprint_name: string,
-  board_name: string,
-  project_name: string,
-  project_source: string,
-  project_status: string,
-  issue_title: string,
-  issue_type: string,
-  issue_description: string,
-  issue_created: string,
-  issue_status: string
-) {
-  let boards: {}[] = [];
-
-  addBoard(board_name, boards);
-  addSprint(boards, sprint_name, board_name);
-  addIssue(
-    boards,
-    issue_title,
-    issue_type,
-    issue_description,
-    issue_created,
-    issue_status,
-    board_name,
-    sprint_name
-  );
-
-  const project_info = {
-    project_source: project_source,
-    project_status: project_status,
-    boards: boards,
-  };
-
-  const project = {
-    project_title: project_name,
-    project_info: project_info,
-  };
-
-  return project;
+  // return for chat use
+  // TODO: REDO
+  return { node_id: node_id, content_id: content_id };
 }
