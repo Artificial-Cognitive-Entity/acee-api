84458d86dda3056020e44f84bb222d7706687005
canadianbees <calzenor@outlook.com>
2024-03-16 15:03:21 -0400

    start update_user api

--- src/app/api/update_user/route.ts
+++ src/app/api/update_user/route.ts
@@ -1,17 +1,29 @@
-import useSWR, { mutate } from 'swr';
-import { User } from '@/app/lib/schemas/user';
+import useSWR, { mutate } from "swr";
+import { User } from "@/app/lib/schemas/user";
+import { connectSingleStore } from "../database/singlestore";
 
+export async function PUT(req: Request) {
+  try {
+    const body = await req.json();
 
-export async function PUT(id: number, data: User) {
-    try {
-     console.log("PUT REQUEST")
+    console.log(body);
 
-      return Response.json({ message: "successful !" }, { status: 200 });
-      } catch (error) {
-        console.error(error);
-    
-        return Response.json({ error: "Internal server error" }, { status: 500 });
-      }
+    console.log("PUT REQUEST");
 
+    const insertRoles = {
+      roles: [body.role],
+    };
 
+    const query = `UPDATE users SET first_name = ${
+      body.first_name
+    }, last_name = ${body.last_name}, status = ${
+      body.status
+    }, role = ${JSON.stringify(insertRoles)} WHERE user_id = ${body.user_id}`;
+
+    return Response.json({ message: "successful !" }, { status: 200 });
+  } catch (error) {
+    console.error(error);
+
+    return Response.json({ error: "Internal server error" }, { status: 500 });
+  }
 }
