24700951ecacd31adca35a7c9858e40ea0a4080d
canadianbees <calzenor@outlook.com>
2024-03-16 15:04:01 -0400

    edit user schema

--- src/app/lib/schemas/user.ts
+++ src/app/lib/schemas/user.ts
@@ -1,13 +1,8 @@
 export interface User{
 user_id: string,
-groups: JSON,
 first_name: string,
 last_name: string,
 email: string,
-password: string,
 status: string,
-session_id: string,
-created_at: string,
-conversations: JSON,
-token: string
+role: string,
 }
