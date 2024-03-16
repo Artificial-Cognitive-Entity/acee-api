90993f46203a8ba2d4aa8eaf99fca2b0cd01b5f3
canadianbees <calzenor@outlook.com>
2024-03-16 15:59:34 -0400

    make update user API

--- src/app/api/update_user/route.ts
+++ src/app/api/update_user/route.ts
@@ -1,25 +1,12 @@
-import useSWR, { mutate } from "swr";
-import { User } from "@/app/lib/schemas/user";
-import { connectSingleStore } from "../database/singlestore";
+import { updateUser } from "../database/singlestore";
 
 export async function PUT(req: Request) {
   try {
     const body = await req.json();
 
-    console.log(body);
-
-    console.log("PUT REQUEST");
-
-    const insertRoles = {
-      roles: [body.role],
-    };
-
-    const query = `UPDATE users SET first_name = ${
-      body.first_name
-    }, last_name = ${body.last_name}, status = ${
-      body.status
-    }, role = ${JSON.stringify(insertRoles)} WHERE user_id = ${body.user_id}`;
-
+    if (!(await updateUser(body))) {
+      return Response.json({ message: "Bad request" }, { status: 400 });
+    }
     return Response.json({ message: "successful !" }, { status: 200 });
   } catch (error) {
     console.error(error);
