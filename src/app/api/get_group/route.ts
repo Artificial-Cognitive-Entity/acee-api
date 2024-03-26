c9430abe5b742ae19884158951694a3123a823ba
Celina Alzenor <calzenor@outlook.com>
2024-03-25 20:34:49 -0400

    uncomment get group api

--- src/app/api/get_group/route.ts
+++ src/app/api/get_group/route.ts
@@ -5,15 +5,13 @@ export async function GET(req: Request) {
   try {
     // TODO: GET LOGGED IN USER'S CREDS
 
-    // const body = req.json()
+    const body = await req.json()
+    const admin_id = body.admin_id
+    
+    const group = await getGroupMembers({ admin_id });
+    console.log(group);
 
-    // const admin_id = body.admin_id
-    // "5a85019f-3aad-44a8-a479-f3e0d371c30f";
-
-    // const group = await getGroupMembers({ admin_id });
-    // console.log(group);
-
-    // return Response.json(group, { status: 200 });
+    return Response.json(group, { status: 200 });
   } catch (error) {
     console.log(error);
     return Response.json({ error: "Internal server error" }, { status: 500 });
