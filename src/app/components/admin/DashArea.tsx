c3afb383916958e64ab6e7775650abc096d8ee8e
canadianbees <calzenor@outlook.com>
2024-03-27 00:17:13 -0400

    add swr and user model, formatting

--- src/app/components/admin/DashArea.tsx
+++ src/app/components/admin/DashArea.tsx
@@ -3,9 +3,14 @@ import UserTable from "./Table/UserTable";
 import Modal from "./Options/Modal";
 import Button from "../button";
 import AddUserIcon from "./CreateUser/AddUserIcon";
+import type { User } from "next-auth";
+import { SWRConfig } from "swr";
 
 type MODALS = "CLOSED" | "CREATE" | "CONFIRM";
-const DashArea = () => {
+const fetcher = (...args: Parameters<typeof fetch>) =>
+  fetch(...args).then((res) => res.json());
+  
+const DashArea = (user: User) => {
   const [showModal, setShowModal] = useState(false);
   const [currentModal, setCurrentModal] = useState<MODALS>("CLOSED");
 
@@ -25,16 +30,16 @@ const DashArea = () => {
 
   return (
     <>
-      <Modal isOpen={!showModal} type={currentModal} />
+      <Modal isOpen={!showModal} type={currentModal} user={user}  />
       <div className="flex justify-center items-center h-screen flex-col">
         <div className="flex flex-col w-full h-full rounded-md">
           <div className="flex justify-between items-center">
-            {/* <div className="bg-primary w-2/12 h-2/12 text-center rounded-md text-lg m-3 p-3 font-bold">
-              Your Group: sdl32
+            <div className="bg-primary w-2/12 h-2/12 text-center rounded-md text-lg m-3 p-3 font-bold">
+              Your Group: {user.group}
             </div>
             <div className="bg-primary w-2/12 h-2/12 text-center rounded-md text-lg m-3 p-3 font-bold">
-              Your Role: Administrator
-            </div> */}
+              Your Role: {user.role}
+            </div>
           </div>
 
           <div className="flex flex-col justify-center items-center h-full gap-5 ml-3 mr-3 mb-12 rounded-md overflow-y-auto">
@@ -57,7 +62,12 @@ const DashArea = () => {
                 </div>
               </div>
               <div className="bg-base-300 rounded-b-md overflow-y-auto">
+                <SWRConfig value={{fetcher}}>
+
                 <UserTable></UserTable>
+                </SWRConfig>
+                
+         
               </div>
             </div>
           </div>
