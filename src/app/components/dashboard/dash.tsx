c6aac15064f835ec22582159c79a40a63e5ce06b
canadianbees <calzenor@outlook.com>
2024-03-27 16:03:18 -0400

    add loading state, edit user session object

--- src/app/components/dashboard/dash.tsx
+++ src/app/components/dashboard/dash.tsx
@@ -2,24 +2,43 @@ import React from "react";
 import Sidebar from "../navigation/sidebar/sidebar";
 import RightSide from "./RightSide";
 import MenuIcon from "../navigation/sidebar/menu";
-import type { User } from "next-auth";
+import { getServerSession, type User } from "next-auth";
+import { options } from "@/app/api/auth/[...nextauth]/options";
 
-const Dash = async (user: User) => {
+const Dash = async () => {
+  const session = await getServerSession(options);
+  const user = session.user;
   return (
-    <div className=" h-full w-full drawer lg:drawer-open">
-      <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-      <div className="drawer-content flex flex-col items-center justify-center bg-base-200">
-        <label
-          htmlFor="my-drawer-2"
-          className="btn btn-red drawer-button lg:hidden self-start"
-        >
-          <MenuIcon />
-        </label>
-        <RightSide role={user.role} id={user.id} group={user.group} email={user.email}  />
-      </div>
-      <div className="drawer-side">
-        <Sidebar role={user.role} id={user.id} group={user.group} email={user.email}></Sidebar>
-      </div>
+    <div className="h-full w-full overflow-y-hidden">
+      {session ? (
+        <div className=" h-full w-full drawer lg:drawer-open">
+          <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+          <div className="drawer-content flex flex-col items-center justify-center bg-base-200">
+            <label
+              htmlFor="my-drawer-2"
+              className="btn btn-red drawer-button lg:hidden self-start"
+            >
+              <MenuIcon />
+            </label>
+            <RightSide
+              role={user.role}
+              id={user.id}
+              group={user.group}
+              email={user.email}
+            />
+          </div>
+          <div className="drawer-side">
+            <Sidebar
+              role={user.role}
+              id={user.id}
+              group={user.group}
+              email={user.email}
+            ></Sidebar>
+          </div>
+        </div>
+      ) : (
+        <h1>you are not logged in</h1>
+      )}
     </div>
   );
 };
