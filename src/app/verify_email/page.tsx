3a5492ca5ca9c64ed5fb896f0b56930fdcae7efe
Kayla Douglas <douglaskayla23@yahoo.com>
2024-03-27 17:21:20 -0400

    verify email and password reset frontend pages added and working

---
+++ src/app/verify_email/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import VEForm from '../components/verify_email/VE'
+
+const VerifyEmail  = () => {
+  return (
+    <VEForm></VEForm>
+  )
+}
+
+export default VerifyEmail
