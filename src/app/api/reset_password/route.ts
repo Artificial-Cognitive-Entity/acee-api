1e1f290a7cfdec07123dfd95cb7223fd6f050d76
canadianbees <calzenor@outlook.com>
2024-03-28 19:19:36 -0400

    fix pass reset and vert email

--- src/app/api/reset_password/route.ts
+++ src/app/api/reset_password/route.ts
@@ -1,80 +1,57 @@
-import bcrypt, { hash } from 'bcrypt';
+import bcrypt, { hash } from "bcrypt";
 import { findToken, updateUserPassword } from "../database/singlestore";
-// Assume connectToSinglestore is imported or defined elsewhere in your project
-// import { connectToSinglestore } from './your-db-connection-file';
-import { redirect } from "next/navigation";
-import { useRouter } from 'next/router';
-const router = useRouter();
 
 const saltRounds = 10; // Recommended salt rounds for bcrypt
 
 export async function POST(req: Request) {
-    if (req.method !== 'POST') {
-        return Response.json({ message: "Method Not Allowed" }, { status: 405 });
+  if (req.method !== "POST") {
+    return Response.json({ message: "Method Not Allowed" }, { status: 405 });
+  }
+
+  const body = await req.json();
+  console.log(body);
+  const url = new URL(body.url);
+  const token = url.searchParams.get("token");
+
+  if (!body.password || !token) {
+    return Response.json(
+      { error: "Password and token are required" },
+      { status: 400 }
+    );
+  }
+
+  try {
+    const user = await findToken({ token: token });
+
+    if (!user) {
+      // No user found with the given verificationToken
+      return Response.json(
+        { error: "Invalid or expired verification token." },
+        { status: 404 }
+      );
     }
 
-    const body = await req.json();
+    // Step 2: Hash newPassword.
+    const hashedPassword = await bcrypt.hash(body.password, saltRounds);
 
-    // Correct way to extract the token from the query string
-    const url = new URL(req.url);
-    const token = url.searchParams.get('token');
+    {
+      const updateData = {
+        password: hashedPassword, // The new hashed password
+        token: token, // The token used for verification
+      };
 
-    // Get the token parameter from the URL
-    //const token = params.get('token');
-    console.log(token);
+      await updateUserPassword(updateData);
 
-    //const userData = body.formData;
-    console.log(body);
-
-    if (!body.email || !body.password || !token) {
-        //res.status(400).json({ error: 'Email is required' });
-        return Response.json(
-            { error: "Password and token are required" },
-            { status: 400 }
-        );
+      return Response.json(
+        { message: "User password reset!" },
+        { status: 200 }
+      );
     }
-
-
-    try {
-
-        const user = await findToken({ token: token });
-
-        if (!user) {
-            // No user found with the given verificationToken
-            return Response.json({ error: "Invalid or expired verification token." }, { status: 404 });
-        }
-
-        // Step 2: Hash newPassword.
-        const hashedPassword = await bcrypt.hash(body.password, saltRounds);
-        console.log(hashedPassword);
-
-        {
-
-            const updateData = {
-                password: hashedPassword, // The new hashed password
-                token: token, // The token used for verification
-            };
-
-            await updateUserPassword(updateData);
-
-
-            //FIX THE REROUTE
-            //router.push('/login');
-            //redirect('/login');
-            return Response.json({ message: "User password reset!" }, { status: 200 });
-            // After your logic and right before sending a response:
-            
-
-
-        }
-
-        // Step 3: Set the new hashed password.
-
-        // Step 4: Respond to the request.
-        return Response.json({ message: "Your password updated successfully." }, { status: 200 });
-
-    } catch (error) {
-        console.error('Error resetting password:', error);
-        return Response.json({ error: "An error occurred during the password reset process." }, { status: 500 });
-    }
+  } catch (error) {
+    console.error("Error resetting password:", error);
+    return Response.json(
+      { error: "An error occurred during the password reset process." },
+      { status: 500 }
+    );
+  }
 }
